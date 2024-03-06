7502dcae2e042e5386aea7b0bf238c954d421c46
s-braitsch <simon.braitsch@novatec-gbmh.de>
2024-03-06 10:47:57 +0100

    day 2

---
+++ aoc_2022/day_02.py
@@ -0,0 +1,39 @@
+from file_parser import file_to_lines
+
+def solve_part_one(lines: list[str]):
+    session_value = 0
+    for line in lines:
+        game_result = 0
+        match line:
+            case 'A X' | 'B Y' | 'C Z':
+                game_result = 3
+            case 'A Z' | 'B X' | 'C Y':
+                game_result = 0
+            case 'A Y' | 'B Z' | 'C X':
+                game_result = 6
+        session_value += game_result + (ord(line[2]) - b'W'[0])
+    return session_value
+
+def solve_part_two(lines: list[str]):
+    session_value = 0
+    for line in lines:
+        game_result = 0
+        picks = []
+        match line[2]:
+            case 'X':
+                game_result = 0
+                picks = [3, 1, 2]
+            case 'Y':
+                game_result = 3
+                picks = [1, 2, 3]
+            case 'Z':
+                game_result = 6
+                picks = [2, 3, 1]
+        session_value += game_result + picks[ord(line[0]) - b'A'[0]]
+    return session_value
+
+
+if __name__ == "__main__":
+    lines = file_to_lines("day_02.txt")
+    print(f"Part 1: {solve_part_one(lines)}")
+    print(f"Part 2: {solve_part_two(lines)}")
--- aoc_2022/file_parser.py
+++ aoc_2022/file_parser.py
@@ -4,7 +4,7 @@ def file_to_lines(file_name: str):
     path = os.path.join('inputs', file_name)
     try:
         with open(path, 'r') as file:
-            lines = file.readlines()
+            lines = file.read().splitlines()
             return lines
     except FileNotFoundError:
         print(f"My brother in Christ, there is no file called {file_name}")
